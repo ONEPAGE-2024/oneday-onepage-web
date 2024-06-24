c35203f028e34b6505dfc0bc307baf8ddb9adad2
sjsojin <qkrsojin@gmail.com>
2024-06-24 10:48:03 +0900

    feat :: router

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,15 +3,14 @@ import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
 import Diary from "../pages/Diary";
 import Create from "../pages/Create";
-
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/" element={<Diary />} />
-        <Route path="/create" element={<Create />} />
+        <Route path="/" element={<Diary />}></Route>
+        <Route path="/create" element={<Create />}></Route>
       </Routes>
     </BrowserRouter>
   );
