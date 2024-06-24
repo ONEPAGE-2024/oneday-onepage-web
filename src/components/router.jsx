d12b88bb68d79a86c07f79a986210aa874a4dd0c
sjsojin <qkrsojin@gmail.com>
2024-06-24 11:01:27 +0900

    feat :: router

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "../pages/Signup";
 import Signin from "../pages/Signin";
-import Main from "../pages/Main";
+import Create from "../pages/Create";
+import Diary from "../pages/Diary";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/" element={<Main />}></Route>
+        <Route path="/create" element={<Create />}></Route>
+        <Route path="/" element={<Diary />}></Route>
       </Routes>
     </BrowserRouter>
   );
