5dd047340581f57b712cfce3a8115e8fd50364d4
0ccssu <128601631+0ccssu@users.noreply.github.com>
2024-06-24 23:53:58 +0900

    Revert "Diary"

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import "./App.css";
-import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Diary from "./pages/Diary";
-import Create from "./pages/Create";
+import React from "react";
+import Router from "./components/router";
 
 function App() {
-  const [diaries, setDiaries] = useState([]);
-
-  const addDiary = (newDiary) => {
-    setDiaries([...diaries, newDiary]);
-  };
-
-  const updateDiary = (updatedDiary) => {
-    setDiaries((prevDiaries) =>
-      prevDiaries.map((diary) =>
-        diary.id === updatedDiary.id ? updatedDiary : diary
-      )
-    );
-  };
-
-  const deleteDiary = (id) => {
-    setDiaries((prevDiaries) => prevDiaries.filter((diary) => diary.id !== id));
-  };
-
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Diary />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router />
+    </div>
   );
 }
 
